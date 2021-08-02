c1f82044ece28edc2670a8e889929632d8fba594
John Puddephatt <john@jdp.org.uk>
2021-08-02 12:28:22 +0100

    search articles placeholder

--- resources/views/forms/search.blade.php
+++ resources/views/forms/search.blade.php
@@ -3,7 +3,8 @@
     {{ _x('Search for:', 'label', 'sage') }}
   </label>
   <input id="search" type="search" class="flex-1 w-1/2 p-2 mr-1 border"
-    placeholder="{!! esc_attr_x('Search&hellip;', 'placeholder', 'sage') !!}" value="{{ get_search_query() }}" name="s">
+    placeholder="{!! esc_attr_x('Search articles&hellip;', 'placeholder', 'sage') !!}" value="{{ get_search_query() }}"
+    name="s">
 
   <button type="submit"
     class="inline-flex px-2 py-2 text-xl font-bold bg-white border-2 appearance-none text-violet-700 border-violet-600 focus:outline-none hover:border-violet-500">
