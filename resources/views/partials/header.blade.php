e34a20088d607d335e7100f1db3bc02a9807cf27
John Puddephatt <john@jdp.org.uk>
2022-03-04 19:14:58 +0530

    fix donate link in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,28 +1,52 @@
 <div class="py-4 text-xs text-center bg-violet-200 md:hidden">
   <div class="container">
-    Your support is vital for our work. <a class="font-bold underline" href="/support">Donate now</a>.
+    Your support is vital for our work.
+    <a class="font-bold underline" href="/donate-migrants-rights/">Donate now</a
+    >.
   </div>
 </div>
 
 @if(!empty($secondaryNavigation))
-<header class="text-gray-800 bg-violet-50 body-font">
-  <div class="container flex flex-col flex-wrap items-center justify-end md:flex-row">
+
+
+<header class="text-gray-800 body-font bg-violet-50">
+  <div
+    class="container flex flex-col flex-wrap items-center justify-end md:flex-row"
+  >
     {!! $secondaryNavigation !!}
   </div>
 </header>
+
+
 @endif
 
 <header class="relative z-20 bg-black body-font">
-  <div class="container flex flex-col flex-wrap items-center px-0 py-4 md:flex-row">
-    <a href="{{ home_url('/') }}" class="flex items-center mb-4 font-extrabold text-gray-100 title-font md:mb-0">
-      <img height="48" width="48" alt="" class="w-12 h-12 mr-2 " src="{{ $siteLogo }}" />
+  <div
+    class="container flex flex-col flex-wrap items-center px-0 py-4 md:flex-row"
+  >
+    <a
+      href="{{ home_url('/') }}"
+      class="flex items-center mb-4 font-extrabold text-gray-100 title-font md:mb-0"
+    >
+      <img
+        height="48"
+        width="48"
+        alt=""
+        class="w-12 h-12 mr-2"
+        src="{{ $siteLogo }}"
+      />
       <span class="ml-1 text-xl">{{ $siteName }}</span>
     </a>
     @if(!empty($primaryNavigation))
+
+
     <nav
-      class="flex flex-wrap items-center justify-center text-xs text-white lowercase 2xl:text-lg md:text-sm lg:text-base md:ml-auto">
+      class="flex flex-wrap items-center justify-center text-xs text-white lowercase md:ml-auto md:text-sm lg:text-base 2xl:text-lg"
+    >
       {!! $primaryNavigation !!}
     </nav>
-    @endif
+
+
+@endif
   </div>
 </header>
