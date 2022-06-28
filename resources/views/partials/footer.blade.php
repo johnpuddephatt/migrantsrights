b957fac0b18076682b3d13787e93f6d0dbda21ff
John Puddephatt <john@jdp.org.uk>
2022-06-28 21:57:22 +0100

    linkedin icon color fix

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
   <div class="bg-violet-200">
     <div class="container flex flex-col items-center py-6 sm:flex-row">
       <h3
-        class="flex-grow hidden py-3 mb-3 text-xl font-extrabold border-b-2 border-violet-700 text-violet-800 md:block lg:mb-0 lg:mr-12 lg:text-xl xl:text-2xl"
+        class="mb-3 hidden flex-grow border-b-2 border-violet-700 py-3 text-xl font-extrabold text-violet-800 md:block lg:mb-0 lg:mr-12 lg:text-xl xl:text-2xl"
       >
         Stay up to date with our work
       </h3>
@@ -13,14 +13,14 @@
           rel="noopener"
           target="_blank"
           href="https://migrantsrights.us2.list-manage.com/subscribe/post?u=d9e512af0e78ae1205ef2bef5&id=a6e14e9a98"
-          class="inline-flex px-2 py-2 mr-1 text-sm font-bold border-2 focus:outline-none border-violet-700 text-violet-800 hover:border-violet-900 hover:text-violet-900 md:px-6 lg:mr-2 lg:text-xl xl:text-2xl"
+          class="focus:outline-none mr-1 inline-flex border-2 border-violet-700 px-2 py-2 text-sm font-bold text-violet-800 hover:border-violet-900 hover:text-violet-900 md:px-6 lg:mr-2 lg:text-xl xl:text-2xl"
           >Join our newsletter</a
         >
 
         <a
           rel="noopener"
           href="/category/newsletters/"
-          class="inline-flex py-2 mr-1 text-xs font-normal leading-tight text-center focus:outline-none text-violet-800 hover:text-violet-900 md:px-3 lg:mr-2"
+          class="focus:outline-none mr-1 inline-flex py-2 text-center text-xs font-normal leading-tight text-violet-800 hover:text-violet-900 md:px-3 lg:mr-2"
           >View all past<br />
           newsletters</a
         >
@@ -87,7 +87,7 @@
         <a
           rel="noopener"
           aria-label="LinkedIn link"
-          class="ml-1 text-gray-300 hover:text-white md:ml-4"
+          class="ml-1 text-violet-700 hover:text-violet-900 md:ml-4"
           href="{{ get_theme_mod('linkedin') }}"
           target="_blank"
         >
@@ -99,24 +99,24 @@
       </span>
     </div>
   </div>
-  <div class="antialiased text-gray-100 bg-black">
+  <div class="bg-black text-gray-100 antialiased">
     <div class="container pt-32 pb-20">
-      <div class="flex flex-wrap order-first gap-8 text-center md:text-left">
-        <div class="flex-shrink w-full mt-3 md:w-1/2 lg:w-1/6">
+      <div class="order-first flex flex-wrap gap-8 text-center md:text-left">
+        <div class="mt-3 w-full flex-shrink md:w-1/2 lg:w-1/6">
           <img
             width="240"
             height="221"
             alt="Migrants’ Rights Network Logo"
-            class="w-1/2 h-auto mx-auto lg:w-full"
+            class="mx-auto h-auto w-1/2 lg:w-full"
             src="{{ $siteLogo }}"
           />
         </div>
-        <div class="flex-shrink w-full mr-auto lg:mt-0 lg:w-1/4 xl:w-1/3">
+        <div class="mr-auto w-full flex-shrink lg:mt-0 lg:w-1/4 xl:w-1/3">
           <p class="mb-3 text-2xl sm:mt-1">{{ $siteStrapline }}</p>
           @if(get_theme_mod('contact_address'))
 
 
-          <p class="mb-4 text-sm site-footer--address contact-address">
+          <p class="site-footer--address contact-address mb-4 text-sm">
             {!! nl2br(strip_tags(get_theme_mod('contact_address'))) !!}
           </p>
 
@@ -127,7 +127,7 @@
 
 
             <a
-              class="block mb-4 site-footer--email contact-email"
+              class="site-footer--email contact-email mb-4 block"
               href="mailto:{{ get_theme_mod('contact_email') }}"
               >{{ get_theme_mod('contact_email') }}</a
             >
@@ -138,7 +138,7 @@
 
 
             <a
-              class="block site-footer--phone contact-phone"
+              class="site-footer--phone contact-phone block"
               href="tel:{{ get_theme_mod('contact_phone') ?? get_theme_mod('contact_phone_human') }}"
               >{{ get_theme_mod('contact_phone_human') ??
               get_theme_mod('contact_phone') }}</a
@@ -151,7 +151,7 @@
           @if(get_theme_mod('company_info'))
 
 
-          <p class="mt-3 text-xs text-gray-300 company-info">
+          <p class="company-info mt-3 text-xs text-gray-300">
             {{ get_theme_mod('company_info') }}
           </p>
 
@@ -162,7 +162,7 @@
 
 
         <div
-          class="flex-shrink w-full md:-ml-6 md:w-1/2 lg:ml-0 lg:w-1/5 xl:w-1/6"
+          class="w-full flex-shrink md:-ml-6 md:w-1/2 lg:ml-0 lg:w-1/5 xl:w-1/6"
         >
           {!! $primaryNavigationFooter !!}
         </div>
